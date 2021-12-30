6b0a71135931f7344a63f6e7516cb2aad2e1d55c
Pasdaran <tarikhadzic313@gmail.com>
2021-12-31 00:20:36 +0100

    My first commit

---
+++ Cooming Soon.html
@@ -0,0 +1,119 @@
+<!Doctype html>
+<html lang="en-US">
+<head>
+	<title>The GAMES</title>
+	<meta name="keywords" content="The Games, Game club, Video igre, Ubisoft, Capcom, EA Games, Marvel">
+	<meta name="description" content="Na ovoj Web stranci možete pogledati sve što vas zanima u vezi najpopularnijih video igara.Ako ste pravi gejmer, onda je ovo pravo mjesto 
+	                                  za vas. Ovdje možete naèi sve potrebne informacije za vašu omiljenu igru, pogledati trailer-e kao i vidjeti da li vaš PC podržava odrepðenu
+									  video igru. Uðite i pogledajte sami">
+	<meta name="author" content="Tarik Hadžiæ">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="Stil.css"/>
+	
+</head>
+<body>
+	<header id="Go Up">
+		<div id="Log_in">
+			<form style="text-align:right;">
+				USERNAME
+				<input name="USERNAME" type="text" size="13" style="border-radius:10px;text-align:center;color:white;background-color:black;">
+				PASSWORD
+				<input type="PASSWORD" name="PASSWORD" size="13" style="border-radius:10px;text-align:center;color:white;background-color:black;"><input type="checkbox" name="" value="">
+				REMEMBER ME
+				<input type="submit" value="LOG IN" style="border-radius:20px 0px 20px 0px;background-color:black;color:white;" >
+			</form>
+		</div>
+		<div id="Logo">
+			<a href="index.html"><img src="Logo.jpg" width="100%" height="153px"></a>
+		</div>
+		<div id="Pozadina">
+			<img src="Z.Pozadina.jpg" width="100%" height="150px">
+		</div>
+		<div id="Search">
+			<input name="USERNAME" type="text" size="15" style="border-radius:20px 0px 20px 0px;width:400px; text-align:center;color:white;background-color:black;">
+			<input type="submit" value="SEARCH" style="border-radius:20px 0px 20px 0px;background-color:black;color:white;" >
+		</div>
+		<nav>
+			<a href="index.html">HOMEPAGE</a>
+			<a href="New Games.html">NEW GAMES</a>
+			<a href="Top Rated.html">TOP RATED</a>
+			<a href="Cooming Soon.html" class="trenutna">COOMING SOON</a>
+			<a href="Registracija.html">REGISTER</a>
+		</nav>
+	</header>
+	<div id="Telo">
+		<aside>
+			<p>CATEGORIES</p>
+			<a href="Action.html">Action</a>
+			<a href="Action-Adventure.html">Action-adventure</a>
+			<a href="Adventure.html">Adventure</a>
+			<a href="Idle Gaming.html">Idle gaming</a>
+			<a href="Racing.html">Racing</a>
+			<a href="Role-playing.html">Role-playing</a>
+			<a href="Shooter.html">Shooter</a>
+			<a href="Simulation.html">Simulation</a>
+			<a href="Strategi.html">Strategi</a>
+			<a href="Sport.html">Sport</a>
+		</aside>
+		<section>
+				<div class="Okvir">
+					<div class="Slika"><a href="Tomb Raider Rise Of The.html" target="_blank"><img src="Tomb Raider Rise Of The.jpg" alt="Gamer" width="100%" height="230px"style="border-style:none; border-radius:30px 0px 30px 0px;" ></a></div>
+					<div class="Text"><a href="Tomb Raider Rise Of The.html" target="_blank">Tomb Raider<br>Rise Of The</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Battlefield Hardline.html" target="_blank"><img src="Battlefield Hardline.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;"></a></div>
+					<div class="Text"><a href="Battlefield Hardline.html" target="_blank">Battlefield<br>Hardline</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Sebastien Loeb Rally Evo.html" target="_blank"><img src="Sebastien Loeb Rally Evo.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;"></a></div>
+					<div class="Text"><a href="Sebastien Loeb Rally Evo.html" target="_blank">Sebastien Loeb<br>Rally Evo</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Gran Turismo 7.html" target="_blank"><img src="Gran Turismo 7.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;" ></a></div>
+					<div class="Text"><a href="Gran Turismo 7.html" target="_blank">Gran Turismo<br>7</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Star Citizen.html" target="_blank"><img src="Star Citizen.jpg" alt="Gamer" width="100%" height="230px"></a></div>
+					<div class="Text"><a href="Star Citizen.html" target="_blank">Star Citizen<br>&nbsp;</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Homefront The Revolution.html" target="_blank"><img src="Homefront The Revolution.jpeg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;"></a></div>
+					<div class="Text"><a href="Homefront The Revolution.html" target="_blank">Homefront<br></a>The Revolution</div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Armored Warfare.html" target="_blank"><img src="Armored Warfare.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;" ></a></div>
+					<div class="Text"><a href="Armored Warfare.html" target="_blank">Armored Warfare<br>2016</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Far Cry Primal.html" target="_blank"><img src="Far Cry Primal.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;"></a></div>
+					<div class="Text"><a href="Far Cry Primal.html" target="_blank">Far Cry<br>Primal</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Mafia 3.html" target="_blank"><img src="Mafia 3.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;" ></a></div>
+					<div class="Text"><a href="Mafia 3.html" target="_blank">Mafia<br>3</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="No Mans Sky.html" target="_blank"><img src="No Mans Sky.jpg" alt="Gamer" width="100%" height="230px"></a></div>
+					<div class="Text"><a href="No Mans Sky.html" target="_blank">No Mans Sky<br>&nbsp;</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="Project Cars.html" target="_blank"><img src="Project Cars.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;"></a></div>
+					<div class="Text"><a href="Project Cars.html" target="_blank">Project Cars<br>2</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika"><a href="FIFA 16.html" target="_blank"><img src="FIFA 16.jpg" alt="Gamer" width="100%" height="230px" style="border-style:none; border-radius:30px 0px 30px 0px;"></a></div>
+					<div class="Text"><a href="FIFA 16.html" target="_blank">FIFA<br>2016</a></div>
+				</div>
+		</section>
+		<div id="Stranice">
+			<a href="#"><p class="Strana">&#x226A; prev</p></a>
+			<a href="Pocetna.html"><p class="Trenutna_strana">1</p></a>
+			<a href="#"><p class="Strana" >next &#8811;</p></a>
+		</div>
+	</div>
+	<footer>
+		<p><b><a href="index.html">Homepage</a><a href="O_nama.html" target="_blank">Contact us</a><a href="#Go Up">Go up</a><b></p>
+		<p class="kraj"><i>&copy; 2016 TheGame. All rights reserved<i></p>
+	</footer>
+</body>
+</html>
---
+++ Top_rated.php
@@ -0,0 +1,180 @@
+<!Doctype html>
+<html lang="en-US">
+<head>
+	<title>The GAMES</title>
+	<meta name="keywords" content="The Games, Game club, Video igre, Ubisoft, Capcom, EA Games, Marvel">
+	<meta name="description" content="Na ovoj Web stranci možete pogledati sve što vas zanima u vezi najpopularnijih video igara.Ako ste pravi gejmer, onda je ovo pravo mjesto 
+	                                  za vas. Ovdje možete naèi sve potrebne informacije za vašu omiljenu igru, pogledati trailer-e kao i vidjeti da li vaš PC podržava odrepðenu
+									  video igru. Uðite i pogledajte sami">
+	<meta name="author" content="Tarik Hadžiæ">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="Stil.css"/>
+	
+</head>
+<body>
+	<header id="Go Up">
+		<div id="Log_in">
+			<form style="text-align:right;">
+				USERNAME
+				<input name="USERNAME" type="text" size="13" style="border-radius:10px;text-align:center;color:white;background-color:black;">
+				PASSWORD
+				<input type="PASSWORD" name="PASSWORD" size="13" style="border-radius:10px;text-align:center;color:white;background-color:black;"><input type="checkbox" name="" value="">
+				REMEMBER ME
+				<input type="submit" value="LOG IN" style="border-radius:20px 0px 20px 0px;background-color:black;color:white;" >
+			</form>
+		</div>
+		<div id="Logo">
+			<a href="index.html"><img src="Logo.jpg" width="100%" height="153px"></a>
+		</div>
+		<div id="Pozadina">
+			<img src="Z.Pozadina.jpg" width="100%" height="150px">
+		</div>
+		<div id="Search">
+			<input name="USERNAME" type="text" size="15" style="border-radius:20px 0px 20px 0px;width:400px; text-align:center;color:white;background-color:black;">
+			<input type="submit" value="SEARCH" style="border-radius:20px 0px 20px 0px;background-color:black;color:white;" >
+		</div>
+		<nav>
+			<a href="index.html">HOMEPAGE</a>
+			<a href="New Games.html">NEW GAMES</a>
+			<a href="Top Rated.html" class="trenutna">TOP RATED</a>
+			<a href="Cooming Soon.html">COOMING SOON</a>
+			<a href="Registracija.html">REGISTER</a>
+		</nav>
+	</header>
+	<div id="Telo">
+		<aside>
+			<p>CATEGORIES</p>
+			<a href="Action.html">Action</a>
+			<a href="Action-Adventure.html">Action-adventure</a>
+			<a href="Adventure.html">Adventure</a>
+			<a href="Idle Gaming.html">Idle gaming</a>
+			<a href="Racing.html">Racing</a>
+			<a href="Role-playing.html">Role-playing</a>
+			<a href="Shooter.html">Shooter</a>
+			<a href="Simulation.html">Simulation</a>
+			<a href="Strategi.html">Strategi</a>
+			<a href="Sport.html">Sport</a>
+		</aside>
+		<section>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Assassins Creed Sindicate.html" target="_blank">
+							<div class="Ocena"><p>9.9</p></div>
+							<img src="Assassins Creed Syndicate.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Assassins Creed Sindicate.html" target="_blank">Assassin's Creed<br>Sindicate</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="The Witcher Wild Hunt.html" target="_blank">
+							<div class="Ocena"><p>9.8</p></div>
+							<img src="The Witcher Wild Hunt.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="The Witcher Wild Hunt.html" target="_blank">The Witcher<br>Wild Hunt</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="GTA V.html" target="_blank">
+							<div class="Ocena"><p>9.7</p></div>
+							<img src="GTA V.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="GTA V.html" target="_blank">GTA<br>V</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Skyrim.html" target="_blank">
+							<div class="Ocena"><p>9.6</p></div>
+							<img src="Skyrim.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Skyrim.html" target="_blank">Skyrim<br>&nbsp;</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Metal Gear Solid The Phantom Pain.html" target="_blank">
+							<div class="Ocena"><p>9.6</p></div>
+							<img src="Metal Gear Solid The Phantom Pain.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Metal Gear Solid The Phantom Pain.html" target="_blank">Metal Gear Solid<br>The Phantom Pain</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="href="Batman Arkham Knight.html" target="_blank">
+							<div class="Ocena"><p>9.5</p></div>
+							<img src="Batman Arkham Knight.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Batman Arkham Knight.html" target="_blank">Batman<br>Arkham Knight</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Tomb Raider Rise Of The.html" target="_blank">
+							<div class="Ocena"><p>9.1</p></div>
+							<img src="Tomb Raider Rise Of The.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Tomb Raider Rise Of The.html" target="_blank">Tomb Raider<br>Rise Of The</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Assassins Creed Black Flag.html" target="_blank">
+							<div class="Ocena"><p>9.0</p></div>
+							<img src="Assassins Creed Black Flag.jpeg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Assassins Creed Black Flag.html" target="_blank">Assassin's Creed<br>Black Flag</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Hit Man Blood Mony.html" target="_blank">
+							<div class="Ocena"><p>9.0</p></div>
+							<img src="HITMAN cetiri.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Hit Man Blood Mony.html" target="_blank">HitMan<br>Blood Money</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Mafia 3.html" target="_blank">
+							<div class="Ocena"><p>8.5</p></div>
+							<img src="Mafia 3.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Mafia 3.html" target="_blank">Mafia<br>3</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Assassins Creed III.html" target="_blank">
+							<div class="Ocena"><p>8.4</p></div>
+							<img src="Assassins Creed III.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Assassins Creed III.html" target="_blank">Assassin's Creed<br>III</a></div>
+				</div>
+				<div class="Okvir">
+					<div class="Slika">
+						<a href="Resident Evil 5.html" target="_blank">
+							<div class="Ocena"><p>8.1</p></div>
+							<img src="Resident Evil 5.jpg" alt="Gamer" width="100%" height="230px" style="background-color:white;border-style:none; border-radius:30px 0px 30px 0px;">
+						</a>	
+					</div>
+					<div class="Text"><a href="Resident Evil 5.html" target="_blank">Resident Evil<br>5</a></div>
+				</div>
+		</section>
+		<div id="Stranice">
+			<a href="#"><p class="Strana">&#x226A; prev</p></a>
+			<a href="Top Rated.html"><p class="Trenutna_strana">1</p></a>
+			<a href="Top Rated2.html"><p class="Strana">2</p></a>
+			<a href="Top Rated2.html"><p class="Strana" >next &#8811;</p></a>
+		</div>
+	</div>
+	<footer>
+		<p><b><a href="index.html">Homepage</a><a href="O_nama.html" target="_blank">Contact us</a><a href="#Go Up">Go up</a><b></p>
+		<p class="kraj"><i>&copy; 2016 TheGame. All rights reserved<i></p>
+	</footer>
+</body>
+</html>
